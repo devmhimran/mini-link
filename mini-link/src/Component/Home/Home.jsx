1d324f0f84c58d576638c6eb902beff705c28fb8
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-26 01:53:55 +0600

    Update Login

--- mini-link/src/Component/Home/Home.jsx
+++ mini-link/src/Component/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className=''>
             <NavMenu></NavMenu>
-            <div className="home__main h-[100.8vh] bg-cover bg-center bg-no-repeat flex items-center justify-center">
+            <div className="home__main content__container">
                 <div className="container mx-auto max-w-screen-xl">
                     <div className="home__heading w-7/12 mx-auto mb-12">
                         <h1 className='text-[#472D2D] text-8xl font-medium capitalize'>
@@ -16,7 +16,7 @@ const Home = () => {
                     </div>
                     <div className="mini__link__input w-7/12 mx-auto flex">
                         <input className='basic__input' type="text" name="" id="" placeholder='Enter Your Favorite Link' />
-                        <button className='text-2xl bg-[#472D2D] text-[#FEFBE7] px-8 py-2'>Shorten</button>
+                        <button className='text-xl bg-[#472D2D] text-[#FEFBE7] px-8 py-2'>Shorten</button>
                     </div>
                 </div>
             </div>
--- mini-link/src/Component/Login/Login.jsx
+++ mini-link/src/Component/Login/Login.jsx
@@ -5,19 +5,21 @@ const Login = () => {
     return (
         <div>
             <NavMenu></NavMenu>
-            <div className="login__main h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center">
+            <div className="login__main content__container">
                 <div className='container w-3/12 border-2 border border-[#472D2D] p-6'>
+                    <h2 className='text-center mb-6 text-3xl font-semibold'>Sign in</h2>
                     <form>
-                        <div className="login__input">
+                        <div className="login__input my-3">
                             <label htmlFor="email" className='capitalize text-lg'>Enter your Email</label>
                             <input className='basic__input' type="text" name="email" id="email" />
                         </div>
-                        <div className="login__input">
+                        <div className="login__input my-3">
                             <label htmlFor="email" className='capitalize text-lg'>Enter your Email</label>
                             <input className='basic__input' type="text" name="email" id="email" />
+                            <small className='text-[#1363DF] font-medium'>Forget Password?</small>
                         </div>
-                        <div className="login__input">
-                            <button>Sign in</button>
+                        <div className="login__input my-3">
+                            <button className='text-lg w-full bg-[#472D2D] text-[#FEFBE7] py-3 font-semibold'>Sign in</button>
                         </div>
                     </form>
                 </div>
