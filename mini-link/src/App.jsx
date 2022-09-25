9dc3f047a62b8d9f45a0fab9b7af053bb4149cb8
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-25 17:20:30 +0600

    Update Home & Login

--- mini-link/src/App.jsx
+++ mini-link/src/App.jsx
@@ -1,28 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import React from 'react'
 import './App.css'
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-} from "@material-tailwind/react";
-import Home from './Component/Home/Home';
 import { Route, Routes } from 'react-router-dom';
-import NavMenu from './Component/NavMenu/NavMenu';
+import Home from './Component/Home/Home';
 import Login from './Component/Login/Login';
 
 function App() {
 
   return (
+    
     <div className="App ">
-      <NavMenu></NavMenu>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+ 
     </div>
+    
   )
 }
 
--- mini-link/src/Component/Login/Login.jsx
+++ mini-link/src/Component/Login/Login.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import NavMenu from '../NavMenu/NavMenu';
 
 const Login = () => {
     return (
         <div>
-            login
+            <NavMenu></NavMenu>
+            <div className="login__main h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center">
+                <div className='container w-3/12 border-2 border border-[#472D2D] p-6'>
+                    <form>
+                        <div className="login__input">
+                            <label htmlFor="email" className='capitalize text-lg'>Enter your Email</label>
+                            <input className='basic__input' type="text" name="email" id="email" />
+                        </div>
+                        <div className="login__input">
+                            <label htmlFor="email" className='capitalize text-lg'>Enter your Email</label>
+                            <input className='basic__input' type="text" name="email" id="email" />
+                        </div>
+                        <div className="login__input">
+                            <button>Sign in</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
+
     );
 };
 
