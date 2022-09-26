960167dc59dcd0f74c279ebfacdf7687c54a0e46
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-09-26 19:19:55 +0600

    Added Link Short Option & Update Input

--- mini-link/src/Component/Home/Home.jsx
+++ mini-link/src/Component/Home/Home.jsx
@@ -1,10 +1,31 @@
-import { Alert } from '@material-tailwind/react';
-import React from 'react';
+import { Result } from 'postcss';
+import React, { useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { toast, Toaster } from 'react-hot-toast';
 import NavMenu from '../NavMenu/NavMenu';
 
 const Home = () => {
+    const [shortenLink, setShortenLink] = useState('');
+    const [copied, setCopied] = useState(false);
+    console.log(shortenLink);
+    const handleLink = (e) => {
+        e.preventDefault();
+        const link = e.target.inputLink.value;
+
+        fetch(`https://api.shrtco.de/v2/shorten?url=${link}`, {
+            method: 'POST'
+        })
+            .then(res => res.json())
+            .then(des => setShortenLink(des))
+
+        console.log(shortenLink);
+    }
+    const handleCopy = (e) => {
+        e.preventDefault();
+        toast.success('Copied Done!')
+    }
     return (
-        <div className=''>
+        <div className='home__container'>
             <NavMenu></NavMenu>
             <div className="home__main content__container">
                 <div className="container mx-auto max-w-screen-xl">
@@ -14,12 +35,35 @@ const Home = () => {
                             Important <span className='font-bold'>Links</span>
                         </h1>
                     </div>
-                    <div className="mini__link__input w-7/12 mx-auto flex">
-                        <input className='basic__input' type="text" name="" id="" placeholder='Enter Your Favorite Link' />
-                        <button className='text-xl bg-[#472D2D] text-[#FEFBE7] px-8 py-2'>Shorten</button>
-                    </div>
+                    {
+                        shortenLink ?
+                            <>
+                                <form onSubmit={handleCopy}>
+                                    <div className="mini__link__input w-7/12 mx-auto flex">
+                                        <input className='basic__input copy__link' type="text" name="inputLink" placeholder='Enter Your Favorite Link' value={shortenLink.result.short_link3} required />
+                                        <CopyToClipboard text={shortenLink.result.short_link3}>
+                                            <button className='text-xl bg-[#472D2D] text-[#FEFBE7] px-8 py-2'>Copy</button>
+                                        </CopyToClipboard>
+
+                                    </div>
+                                </form>
+                            </>
+                            :
+                            <>
+                                <form onSubmit={handleLink}>
+                                    <div className="mini__link__input w-7/12 mx-auto flex">
+                                        <input className='basic__input' type="text" name="inputLink" placeholder='Enter Your Favorite Link' required />
+                                        <button className='text-xl bg-[#472D2D] text-[#FEFBE7] px-8 py-2'>Shorten</button>
+                                    </div>
+                                </form>
+                            </>
+                    }
                 </div>
             </div>
+            <Toaster
+                position="top-center"
+                reverseOrder={false}
+            />
         </div>
     );
 };
--- mini-link/src/Component/NavMenu/NavMenu.jsx
+++ mini-link/src/Component/NavMenu/NavMenu.jsx
@@ -7,8 +7,8 @@ const NavMenu = () => {
     return (
         <div className='container mx-auto w-3/6 mb-[-100px]'>
             <div className="navbar__main flex justify-between items-center pt-3">
-                <Link to='/'><img src={miniLinkLogo} alt="" /></Link>
-                <Link to='/signin' className='text-2xl font-medium'>Sign in</Link>
+                <a href="/"><img src={miniLinkLogo} alt="" /></a>
+                <Link to='/signin' className='text-2xl font-medium text-[#472D2D]'>Sign in</Link>
             </div>
         </div>
     );
